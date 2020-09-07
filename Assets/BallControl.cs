857dfe5a9f2ac9648f0396ada382a0bf61290c00
Emre Emiroğlu <62201257+Emre-Emiroglu@users.noreply.github.com>
2020-09-07 17:37:32 +0300

    Initial commit

---
+++ Assets/BallControl.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    public enum Status
+    {
+        Ready,
+        Fail,
+        Success
+    }
+    public Status status;
+    public bool touchball;
+    public bool success;
+    Rigidbody rb;
+    LineRenderer line;
+    public float rotatespeed;
+    public float shotpower;
+    public int combo;
+    float xrot;
+    float yrot;
+    GameMaster gamemaster;
+    Vector3 startpos;
+    public bool startaccess;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gamemaster = GameObject.Find("GameMaster").GetComponent<GameMaster>();
+        startaccess = false;
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+        line = GetComponent<LineRenderer>();
+        line.enabled = false;
+        status = Status.Ready;
+        startpos = transform.position;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        startaccess = gamemaster.start;
+        if (startaccess)
+        {
+            MouseWork();
+        }
+    }
+    void MouseWork()
+    {
+        if (Input.GetMouseButton(0) && touchball)
+        {
+            xrot -= Input.GetAxis("Mouse X") * rotatespeed;
+            yrot += Input.GetAxis("Mouse Y") * rotatespeed;
+            transform.rotation = Quaternion.Euler(yrot, xrot, 0);
+            line.enabled = true;
+            line.SetPosition(0, transform.position);
+            line.SetPosition(1, transform.position + transform.forward * 4);
+            if (yrot < -50)
+            {
+                yrot = -50;
+            }
+        }
+        if (Input.GetMouseButtonUp(0) && touchball)
+        {
+            Shooting();
+        }
+    }
+    void Shooting()
+    {
+        rb.velocity = transform.forward * shotpower;
+        line.enabled = false;
+        touchball = false;
+        rb.useGravity = true;
+        StartCoroutine(StatusRefresh());
+    }
+    private void OnMouseDown()
+    {
+        touchball = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "hole")
+        {
+            status = Status.Success;
+            success = true;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag=="ground" && success==false)
+        {
+            status = Status.Fail;
+        }
+    }
+    IEnumerator StatusRefresh()
+    {
+        yield return new WaitForSeconds(4.0f);
+        gamemaster.shotcount++;
+        if (status==Status.Fail)
+        {
+            combo = 0;
+        }
+        if (status == Status.Success)
+        {
+            gamemaster.successshot++;
+            combo++;
+            gamemaster.currentscore += combo;
+        }
+        status = Status.Ready;
+        transform.position = startpos;
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+        rb.useGravity = false;
+        rb.velocity = Vector3.zero;
+        success = false;
+        line.SetPosition(0, new Vector3(0, 0, 0));
+        line.SetPosition(1, new Vector3(0, 0, 1));
+    }
+}
---
+++ Assets/GameMaster.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class GameMaster : MonoBehaviour
+{
+    public GameObject startpanel, gameoverpanel, winpanel, ingamepanel;
+    public TextMeshProUGUI wingamescore;
+    public TextMeshProUGUI losegamescore;
+    public TextMeshProUGUI ballcount;
+    public int currentscore;
+    public int successshot;
+    public int shotcount;
+    public GameObject[] balls;
+    public bool start;
+    // Start is called before the first frame update
+    void Start()
+    {
+        start = false;
+        startpanel.SetActive(true);
+        gameoverpanel.SetActive(false);
+        winpanel.SetActive(false);
+        ingamepanel.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        ballcount.text = "X " + (5 - shotcount); 
+    }
+    private void LateUpdate()
+    {
+        ChechkPlayerShoot();
+    }
+    void ChechkPlayerShoot()
+    {
+        if (successshot!=0)
+        {
+            balls[successshot - 1].gameObject.GetComponent<Image>().color = Color.green;
+        }
+        if (shotcount==5 && successshot<3)
+        {
+            GameOver();
+        }
+        if (successshot==3)
+        {
+            Win();
+        }
+    }
+    void GameOver()
+    {
+        start = false;
+        startpanel.SetActive(false);
+        gameoverpanel.SetActive(true);
+        winpanel.SetActive(false);
+        ingamepanel.SetActive(false);
+        losegamescore.text = "Score: " + currentscore;
+    }
+    void Win()
+    {
+        start = false;
+        startpanel.SetActive(false);
+        gameoverpanel.SetActive(false);
+        winpanel.SetActive(true);
+        ingamepanel.SetActive(false);
+        wingamescore.text = "Score: " + currentscore;
+    }
+    public void StartButton()
+    {
+        start = true;
+        startpanel.SetActive(false);
+        ingamepanel.SetActive(true);
+    }
+    public void Restart()
+    {
+        successshot = 0;
+        currentscore = 0;
+        shotcount = 0;
+        GameObject.FindGameObjectWithTag("Player").GetComponent<BallControl>().combo = 0;
+        GameObject.FindGameObjectWithTag("Player").GetComponent<BallControl>().touchball = false;
+        start = true;
+        startpanel.SetActive(false);
+        ingamepanel.SetActive(true);
+        gameoverpanel.SetActive(false);
+        winpanel.SetActive(false);
+        foreach (GameObject item in balls)
+        {
+            item.gameObject.GetComponent<Image>().color = Color.white;
+        }
+    }
+}
